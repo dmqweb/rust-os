f18c415283ad4ca9d4b71d75103168694767b78f
dmq <3267807514@qq.com>
2025-04-20 02:59:07 +0800

    _start函数作为入口

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #[!no_std] //禁用标准库
-#[!no_main] //覆盖默认入口点
+#[!no_main] //覆盖默认入口点，因为没有调用它的底层运行时
 // 禁用标准库之后，需要添加编译器在panic时应该调用的函数
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info:&PanicInfo)->!{
     loop {}
 }
-fn main(){
+#[unsafe(no_mangle)]//确保函数名不被编译器改变
+pub extern "C" fn _start() ->!{//类unix操作系统以_start作为入口名称
+    loop {
+
+    }
 }
