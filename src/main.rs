ef8b5b056cb639533859d467ebc9a39ad71b4e5e
dmq <3267807514@qq.com>
2025-04-20 17:29:14 +0800

    处理vga字符缓冲区

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ use core::panic::PanicInfo;
 fn panic(_info:&PanicInfo)->!{
     loop {}
 }
+static HELLO:&[u8] = b"Hello World";
 #[unsafe(no_mangle)]//确保函数名不被编译器改变
 pub extern "C" fn _start() ->!{//类unix操作系统以_start作为入口名称
+    // vga字符缓冲区允许操作系统或程序直接操作内存，直接控制显存中的字符和样式，而无需复杂的图形操作
+    let vga_buffer = 0xb8000 as *mut u8;//vga缓冲区的地址,并转换为裸指针
+    for (i,&byte) in HELLO.iter().enumerate(){//迭代字节
+        unsafe {
+            // 解引用并将字符串的每个字节赋值给裸指针偏移位置
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // 添加前景色oxb表示谈青色
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {
 
     }
