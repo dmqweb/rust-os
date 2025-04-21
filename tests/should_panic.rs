855569d8faad73417965e2f73ed8daf8dbf0f673
dmq <3267807514@qq.com>
2025-04-21 16:14:07 +0800

    简化should_panic程序逻辑

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,34 +1,22 @@
 // #[should_panic]属性需要标准库支持，因此创建一个集成测试进行支持
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
-use blog_os::{QemuExitCode, exit_qemu, serial_println};
+use blog_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+fn should_fail() {
+    serial_print!("should_fail... ");
+    assert_eq!(0, 1);
+}
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
     loop {}
-}
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    test_main();
-    loop {}
-}
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-use blog_os::serial_print;
-#[test_case]
-fn should_fail() {
-    serial_print!("should_fail... ");
-    assert_eq!(0, 1);
 }
